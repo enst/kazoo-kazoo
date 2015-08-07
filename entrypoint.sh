2f78c3eaad172769fc082b341faab6e480cb0624
Bing Li <enst.bupt@gmail.com>
2015-08-06 22:23:01 -0400

    rabbitmq listens on 127.0.0.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ sed -i '/port/s/15984/5984/' /etc/kazoo/config.ini
 sed -i '/port/s/15986/5986/' /etc/kazoo/config.ini 
 sed -i "/cookie/s/change_me/$COOKIE/" /etc/kazoo/config.ini 
 
+sed -i '/NODE_IP_ADDRESS/s/0.0.0.0/127.0.0.1/' /etc/kazoo/rabbitmq/rabbitmq-env.conf 
+
 service rabbitmq-server restart 
 sleep 5
 service kz-whistle_apps restart 
