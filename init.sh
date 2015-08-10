c01c7ced3e51acca4c9846727b9f8cd8e50f2908
Bing Li <enst.bupt@gmail.com>
2015-08-09 20:09:22 -0400

    reinstall->install   monster-ui

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 MY_IP=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
 
-yum reinstall -y monster-ui-accounts monster-ui-core monster-ui-numbers \
+yum install -y monster-ui-accounts monster-ui-core monster-ui-numbers \
                     monster-ui-pbxs monster-ui-voip monster-ui-webhooks 
 
 sup crossbar_maintenance init_apps '/var/www/html/monster-ui/apps' "http://${MY_IP}:8000/v2"
